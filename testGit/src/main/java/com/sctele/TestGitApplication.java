ebc5c34f94cce8defd4b49301d1a417c4a1cf429
wenlei <jackyweon@126.com>
2018-05-29 15:14:05 +0800

    提交代码

---
+++ testGit/src/main/java/com/sctele/TestGitApplication.java
@@ -0,0 +1,13 @@
+package com.sctele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApplication {
+
+	public static void main(String[] args) {
+		System.out.println("这是主干的代码");
+		SpringApplication.run(TestGitApplication.class, args);
+	}
+}
